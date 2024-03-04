206e9ec433a082450dd2bb5b5748c829c5974464
syed imam <syedimam1998@gmail.com>
2024-03-04 15:43:59 +0530

    simple pagination completed

--- Page-Frontend/src/App.jsx
+++ Page-Frontend/src/App.jsx
@@ -1,34 +1,53 @@
 import React, { useEffect, useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import axios from "axios";
 
 function App() {
   const [data, setData] = React.useState([]);
-  const [pageNum,setPageNum]=useState(1);
+  const [hasPrev, setHasPrev] = React.useState(false);
+  const [hasNext, setHasNext] = React.useState(false);
+  const [pageNum, setPageNum] = useState(1);
   useEffect(() => {
     const fetchData = async () => {
-      const data = await axios.get("http://localhost:4000/getData/?page="+pageNum);
+      const data = await axios.get(
+        "http://localhost:4000/getData/?page=" + pageNum
+      );
       console.log("data.data", data.data);
-      setData(data.data.data)
+      setHasPrev(data.data.hasPreviousPage);
+      setHasNext(data.data.hasNextPage);
+      setData(data.data.data);
     };
 
     fetchData();
   }, [pageNum]);
 
+  const loadData = (operation) => {
+    if (operation === "prev") {
+      setPageNum(pageNum - 1);
+    } else if (operation === "next") {
+      setPageNum(pageNum + 1);
+    }
+  };
   return (
     <div>
       <div>
         {data.length > 0 &&
-          data.map((item,index) => {
+          data.map((item, index) => {
             return <div key={index}>{item.name}</div>;
           })}
       </div>
       <br></br>
       <div className="pagination">
-        <label  onClick={()=>setPageNum(1)}>1</label>
-        <label onClick={()=>setPageNum(2)}>2</label>
+        {hasPrev && (
+          <div className="paginationBtn" onClick={() => loadData("prev")}>
+            Previous
+          </div>
+        )}
+        {hasNext && (
+          <div className="paginationBtn" onClick={() => loadData("next")}>
+            Next
+          </div>
+        )}
       </div>
     </div>
   );
